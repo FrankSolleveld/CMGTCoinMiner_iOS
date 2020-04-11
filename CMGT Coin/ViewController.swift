d4a6c8045584b13e2aecbd584755af2bb376ca6f
IAmMisterFrank <fsolleveld@outlook.com>
2020-04-11 17:18:37 +0200

    Initial Commit

---
+++ CMGT Coin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CMGT Coin
+//
+//  Created by Frank Solleveld on 11/04/2020.
+//  Copyright © 2020 Frank Solleveld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
