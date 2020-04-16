fa477332e6f4b1968f7677773392b10fea88b4b0
IAmMisterFrank <fsolleveld@outlook.com>
2020-04-16 09:55:29 +0200

    Added layout in storyboard for Coin Mine VC, added empty VC Code

---
+++ CMGT Coin/Controllers/MinerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MinerViewController.swift
+//  CMGT Coin
+//
+//  Created by Frank Solleveld on 16/04/2020.
+//  Copyright © 2020 Frank Solleveld. All rights reserved.
+//
+
+import UIKit
+
+class MinerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
